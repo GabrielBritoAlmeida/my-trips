d9432f7e119ecd9789bfe1728f8d595dbab18daa
gabriel.brito <gabriefranccelino@gmail.com>
2021-03-14 17:19:45 -0300

    refactor: manifest and app, for My trips

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,14 +9,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest.json" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
-          content="A simple project starter to work with Typescript, React, NextJs and Styled-Components"
+          content="A simple project to show my favorite spots in the word."
         />
       </Head>
       <GlobalStyles />
